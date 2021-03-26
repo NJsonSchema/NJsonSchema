603268e97cd2013f0d95f7edddc862d037665bd2
Marko Lahma <marko.lahma@gmail.com>
2021-03-26 18:04:32 +0200

    Reduce memory allocations
    * remove usage Enum.HasFlag
    * don't do unnecessary string.Splits
    * don't enter regexp paths when not needed
    * prefer concrete types when possible
    * don't capture closures in LINQ

--- src/NJsonSchema/ConversionUtilities.cs
+++ src/NJsonSchema/ConversionUtilities.cs
@@ -7,6 +7,7 @@
 //-----------------------------------------------------------------------
 
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Xml.Linq;
 
@@ -54,7 +55,7 @@ namespace NJsonSchema
                 return string.Empty;
             }
 
-            input = ConvertDashesToCamelCase((input[0].ToString().ToUpperInvariant() + (input.Length > 1 ? input.Substring(1) : ""))
+            input = ConvertDashesToCamelCase(Capitalize(input)
                 .Replace(" ", "_")
                 .Replace("/", "_"));
 
@@ -64,6 +65,20 @@ namespace NJsonSchema
             }
 
             return input;
+        }
+
+        [MethodImpl((MethodImplOptions) 256)]
+        private static string Capitalize(string input)
+        {
+            if (char.IsUpper(input[0]))
+            {
+                return input;
+            }
+            if (input.Length == 1)
+            {
+                return char.ToUpperInvariant(input[0]).ToString();
+            }            
+            return char.ToUpperInvariant(input[0]) + input.Substring(1);
         }
 
         /// <summary>Converts the string to a string literal which can be used in C# or TypeScript code.</summary>
